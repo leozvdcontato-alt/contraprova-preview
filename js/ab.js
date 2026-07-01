ca977fc55b21fc2dccd25ad909916cee23f0ea9a
leozvdcontato-alt <leozvd.contato@gmail.com>
2026-07-01 00:20:23 +0000

    update postos

---
+++ js/ab.js
@@ -0,0 +1,13 @@
+const bairros = {
+
+    centro: {
+        total: 8,
+        mapa: "https://www.google.com/maps?q=Centro,Niterói,RJ&output=embed"
+    },
+
+    icarai: {
+        total: 4,
+        mapa: "https://www.google.com/maps?q=Icaraí,Niterói,RJ&output=embed"
+    }
+
+};
